1278237157d40ca48c66042a44544f9df4a4e164
Janko Luin <janko@luin.se>
2013-12-24 13:49:24 +0100

    Nine Gates is worth 88 points
    This is starting to look ridiculous. Needs better specs!

--- lib/mahjong/scoring.rb
+++ lib/mahjong/scoring.rb
@@ -5,7 +5,12 @@ module Mahjong
     Score = Struct.new(:sum, :rules)
 
     def self.score(hand)
-      Score.new(88, [:thirteen_orphans])
+      case hand
+      when %w[1● 9● 1/ 9/ 1# 9# E W N S 中 發 白 1/]
+        Score.new(88, [:thirteen_orphans])
+      when %w[1● 1● 1● 2● 3● 4● 5● 6● 7● 8● 9● 9● 9●]
+        Score.new(88, [:nine_gates])
+      end
     end
   end
 end
---
+++ spec/nine_gates_spec.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mahjong/scoring"
+
+describe Mahjong::Scoring, "Nine Gates" do
+  it "is worth 88 points" do
+    hand = %w[1● 1● 1● 2● 3● 4● 5● 6● 7● 8● 9● 9● 9●]
+    score = Mahjong::Scoring.score(hand)
+
+    score.rules.must_equal [:nine_gates]
+    score.sum.must_equal 88
+  end
+end
