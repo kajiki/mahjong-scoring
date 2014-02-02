51a4d4de9cfec5ea60d96544f098ab15189fbeb0
Janko Luin <janko@luin.se>
2014-02-02 21:32:25 +0100

    Use RSpec colors by default in this project
    Some say it should be a ~/.rspec setting. I say it should be the
    default. Seriously.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
 require "rspec"
 require "mahjong_scoring"
 
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :progress # :documentation, :progress, :html, :textmate
+end
