a0f397d30d2bdf9b5cbcd862f3d8648ba7ffa68c
Jessica Hanmeier <jessica@esperanto.se>
2014-02-08 18:47:42 +0100

    Distinguish between terminals, honors etc.

--- lib/mahjong_scoring/models/tile.rb
+++ lib/mahjong_scoring/models/tile.rb
@@ -5,27 +5,42 @@ class Tile
     "•" => :circle
   }.freeze
 
-  HONORS = {
+  DRAGONS = {
     "B" => :white_dragon, "白" => :white_dragon,
     "C" => :red_dragon,   "中" => :red_dragon,
-    "F" => :green_dragon, "發" => :green_dragon,
+    "F" => :green_dragon, "發" => :green_dragon
+  }.freeze
+
+  WINDS = {
     "E" => :east_wind,    "東" => :east_wind,
     "S" => :south_wind,   "南" => :south_wind,
     "W" => :west_wind,    "西" => :west_wind,
     "N" => :north_wind,   "北" => :north_wind
-  }
+  }.freeze
 
   attr_reader :rank, :type
 
   def initialize(string)
-    if HONORS.has_key? string
-      @type = HONORS[string]
+    if DRAGONS.has_key? string
+      @dragon = true
+      @type = DRAGONS[string]
+    elsif WINDS.has_key? string
+      @wind = true
+      @type = WINDS[string]
     else
       rank, type = string.chars
       @rank = numerical_rank(rank)
       @type = SUITS[type]
     end
+    @string_representation = string
   end
+
+  def terminal?() rank == 1 or rank == 9 end
+  def honor?() dragon? or wind? end
+  def dragon?() !!@dragon end
+  def wind?() !!@wind end
+
+  def to_s() @string_representation end
 
   private
   def numerical_rank(candidate)
--- spec/models/tile_spec.rb
+++ spec/models/tile_spec.rb
@@ -2,11 +2,14 @@ require "spec_helper"
 
 shared_examples_for "suited tile" do |options|
   (1..9).each do |rank|
-    it "can have the rank #{rank}" do
-      tile = Tile.new("#{rank}#{options[:suit]}")
+    describe "rank #{rank}" do
+      subject(:tile) { Tile.new("#{rank}#{options[:suit]}") }
 
-      tile.type.should == options[:expected_type]
-      tile.rank.should == rank
+      its(:type) { should == options[:expected_type] }
+      its(:rank) { should == rank }
+      it { should_not be_honor }
+      it { should_not be_dragon }
+      it { should_not be_wind }
     end
   end
 
@@ -14,6 +17,12 @@ shared_examples_for "suited tile" do |options|
     expect { Tile.new("0#{options[:suit]}") }.to raise_error(ArgumentError)
     expect { Tile.new(" #{options[:suit]}") }.to raise_error(ArgumentError)
     expect { Tile.new("X#{options[:suit]}") }.to raise_error(ArgumentError)
+  end
+
+  it "is terminal when rank is 1 or 9" do
+    Tile.new("1#{options[:suit]}").should be_terminal
+    Tile.new("9#{options[:suit]}").should be_terminal
+    Tile.new("5#{options[:suit]}").should_not be_terminal
   end
 end
 
@@ -36,39 +45,58 @@ shared_examples_for "honor tile" do |options|
 
   its(:rank) { should be_nil }
   its(:type) { should == options[:expected_type] }
+  it { should_not be_terminal }
+  it { should be_honor }
 end
 
+shared_examples_for "dragon tile" do |options|
+  subject { Tile.new(options[:symbol]) }
+
+  it_behaves_like "honor tile", options
+  it { should be_dragon }
+  it { should_not be_wind }
+end
+
+shared_examples_for "wind tile" do |options|
+  subject { Tile.new(options[:symbol]) }
+
+  it_behaves_like "honor tile", options
+  it { should be_wind }
+  it { should_not be_dragon }
+end
+
+
 describe "White Dragon tile" do
-  it_behaves_like "honor tile", symbol: "B", expected_type: :white_dragon
-  it_behaves_like "honor tile", symbol: "白", expected_type: :white_dragon
+  it_behaves_like "dragon tile", symbol: "B", expected_type: :white_dragon
+  it_behaves_like "dragon tile", symbol: "白", expected_type: :white_dragon
 end
 
 describe "Red Dragon tile" do
-  it_behaves_like "honor tile", symbol: "C", expected_type: :red_dragon
-  it_behaves_like "honor tile", symbol: "中", expected_type: :red_dragon
+  it_behaves_like "dragon tile", symbol: "C", expected_type: :red_dragon
+  it_behaves_like "dragon tile", symbol: "中", expected_type: :red_dragon
 end
 
 describe "Green Dragon tile" do
-  it_behaves_like "honor tile", symbol: "F", expected_type: :green_dragon
-  it_behaves_like "honor tile", symbol: "發", expected_type: :green_dragon
+  it_behaves_like "dragon tile", symbol: "F", expected_type: :green_dragon
+  it_behaves_like "dragon tile", symbol: "發", expected_type: :green_dragon
 end
 
 describe "East Wind tile" do
-  it_behaves_like "honor tile", symbol: "E", expected_type: :east_wind
-  it_behaves_like "honor tile", symbol: "東", expected_type: :east_wind
+  it_behaves_like "wind tile", symbol: "E", expected_type: :east_wind
+  it_behaves_like "wind tile", symbol: "東", expected_type: :east_wind
 end
 
 describe "South Wind tile" do
-  it_behaves_like "honor tile", symbol: "S", expected_type: :south_wind
-  it_behaves_like "honor tile", symbol: "南", expected_type: :south_wind
+  it_behaves_like "wind tile", symbol: "S", expected_type: :south_wind
+  it_behaves_like "wind tile", symbol: "南", expected_type: :south_wind
 end
 
 describe "West Wind tile" do
-  it_behaves_like "honor tile", symbol: "W", expected_type: :west_wind
-  it_behaves_like "honor tile", symbol: "西", expected_type: :west_wind
+  it_behaves_like "wind tile", symbol: "W", expected_type: :west_wind
+  it_behaves_like "wind tile", symbol: "西", expected_type: :west_wind
 end
 
-describe Tile, "North Wind tile" do
-  it_behaves_like "honor tile", symbol: "N", expected_type: :north_wind
-  it_behaves_like "honor tile", symbol: "北", expected_type: :north_wind
+describe "North Wind tile" do
+  it_behaves_like "wind tile", symbol: "N", expected_type: :north_wind
+  it_behaves_like "wind tile", symbol: "北", expected_type: :north_wind
 end
