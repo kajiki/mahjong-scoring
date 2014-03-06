bbf3e54b71fcc88361afb50c5a5b991dfe1c70eb
Jessica Hanmeier <jessica@esperanto.se>
2014-03-06 19:33:13 +0100

    Cleanup of Half Flush

--- lib/mahjong_scoring/rules/half_flush.rb
+++ lib/mahjong_scoring/rules/half_flush.rb
@@ -3,23 +3,9 @@ module Rules
     def self.score() 6 end
 
     def self.match?(hand)
-      super and (
-        (
-          hand.tiles.all? { |t| t.type == :bamboo or t.honor? } and
-          hand.tiles.any? { |t| t.type == :bamboo } and
-          hand.tiles.any? { |t| t.honor? }
-        ) or
-        (
-          hand.tiles.all? { |t| t.type == :circle or t.honor? } and
-          hand.tiles.any? { |t| t.type == :circle } and
-          hand.tiles.any? { |t| t.honor? }
-        ) or
-        (
-          hand.tiles.all? { |t| t.type == :character or t.honor? } and
-          hand.tiles.any? { |t| t.type == :character } and
-          hand.tiles.any? { |t| t.honor? }
-        )
-      )
+      super &&
+        hand.tiles.select(&:suited?).map(&:type).uniq.count == 1 &&
+        hand.tiles.any? { |t| t.honor? }
     end
   end
 end
--- spec/rules/half_flush_spec.rb
+++ spec/rules/half_flush_spec.rb
@@ -9,14 +9,20 @@ describe Rules::HalfFlush do
     should match_hand ["1/ 1/ 1/", "3/ 4/ 5/", "2/ 2/ 2/", "E E E", "8/ 8/"]
     should match_hand ["3• 3• 3•", "4• 5• 6•", "B B B", "6• 7• 8•", "9• 9•"]
     should match_hand ["1# 1# 1#", "3# 4# 5#", "N N N N", "6# 7# 8#", "6# 6#"]
-
-    should_not match_hand ["E E E", "3# 4# 5#", "2# 2# 2#", "8# 8#"]
-    should_not match_hand ["1• 1• 1•", "3# 4# 5#", "2/ 2/ 2/", "6/ 7/ 8/", "8/ 8/"]
+    should match_hand ["E E E", "C C C", "N N N N", "B B B", "6# 6#"]
   end
 
   it "rejects hands without honors" do
     should_not match_hand ["1/ 1/ 1/", "3/ 4/ 5/", "2/ 2/ 2/", "6/ 7/ 8/", "8/ 8/"]
     should_not match_hand ["3• 3• 3•", "4• 5• 6•", "2• 2• 2•", "6• 7• 8•", "9• 9•"]
     should_not match_hand ["1# 1# 1#", "3# 4# 5#", "2# 2# 2# 2#", "6# 7# 8#", "6# 6#"]
+  end
+
+  it "rejects hands with more than one suit present" do
+    should_not match_hand ["1# 1# 1#", "3# 4# 5#", "2/ 2/ 2/", "6/ 7/ 8/", "8/ 8/"]
+  end
+
+  it "rejects invalid hands" do
+    should_not match_hand ["E E E", "3# 4# 5#", "2# 2# 2#", "8# 8#"]
   end
 end
